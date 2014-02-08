0c486a9b3bb101e739dc6097502d54b7896ef052
Steve Jamesson <steve@groovywebdesign.com>
2014-02-08 11:58:31 -0500

    initial commit

--- lib/config.php
+++ lib/config.php
@@ -11,7 +11,7 @@ add_theme_support('jquery-cdn');            // Enable to load jQuery from the Go
 /**
  * Configuration values
  */
-define('GOOGLE_ANALYTICS_ID', ''); // UA-XXXXX-Y (Note: Universal Analytics only, not Classic Analytics)
+define('GOOGLE_ANALYTICS_ID', 'UA-32560183-1'); // UA-XXXXX-Y (Note: Universal Analytics only, not Classic Analytics)
 define('POST_EXCERPT_LENGTH', 40); // Length in words for excerpt_length filter (http://codex.wordpress.org/Plugin_API/Filter_Reference/excerpt_length)
 
 /**
@@ -20,7 +20,7 @@ define('POST_EXCERPT_LENGTH', 40); // Length in words for excerpt_length filter 
 function roots_main_class() {
   if (roots_display_sidebar()) {
     // Classes on pages with the sidebar
-    $class = 'col-sm-8';
+    $class = 'col-xs-12 col-sm-9 col-sm-pull-3 col-md-9 col-md-pull-3 col-lg-9 col-lg-pull-3';
   } else {
     // Classes on full width pages
     $class = 'col-sm-12';
@@ -33,7 +33,7 @@ function roots_main_class() {
  * .sidebar classes
  */
 function roots_sidebar_class() {
-  return 'col-sm-4';
+  return 'col-xs-12 col-sm-3 col-sm-push-9 col-md-3 col-md-push-9 col-lg-3 col-lg-push-9';
 }
 
 /**
@@ -62,7 +62,7 @@ function roots_display_sidebar() {
      * Any of these page templates that return true won't show the sidebar
      */
     array(
-      'template-custom.php'
+      'template-full-width.php'
     )
   );
 
--- lib/custom.php
+++ lib/custom.php
@@ -2,3 +2,24 @@
 /**
  * Custom functions
  */
+
+
+// load favicon
+add_action('wp_head', 'smoky_load_favicon');
+function smoky_load_favicon()
+{
+  ?>
+  <link rel="icon" href="<?php echo home_url() ?>/favicon.ico">
+  <link rel="SHORTCUT ICON" href="<?php echo home_url() ?>/favicon.ico">
+  <?php
+}
+
+
+// load custom fonts
+add_action('wp_enqueue_styles', 'smoky_load_fonts');
+function smoky_load_fonts()
+{
+  wp_register_style('gfont', '#URL-to-google-fonts#');
+
+  wp_enqueue_style('gfont');
+}
