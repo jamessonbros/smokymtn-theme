bdf700e08eb1502a0a4ecd47459247b494d4d1d4
Steve Jamesson <steve@groovywebdesign.com>
2014-02-08 12:17:42 -0500

    load apple icons <link> tags

--- lib/custom.php
+++ lib/custom.php
@@ -5,12 +5,21 @@
 
 
 // load favicon
-add_action('wp_head', 'smoky_load_favicon');
-function smoky_load_favicon()
+add_action('wp_head', 'smoky_load_icons');
+function smoky_load_icons()
 {
   ?>
   <link rel="icon" href="<?php echo home_url() ?>/favicon.ico">
   <link rel="SHORTCUT ICON" href="<?php echo home_url() ?>/favicon.ico">
+  <link rel="apple-touch-icon" href="<?php echo get_stylesheet_directory_uri() ?>/assets/img/apple-touch-icon.png">
+  <link rel="apple-touch-icon" sizes="57x57" href="<?php echo get_stylesheet_directory_uri() ?>/assets/img/apple-touch-icon-57x57.png">
+  <link rel="apple-touch-icon" sizes="72x72" href="<?php echo get_stylesheet_directory_uri() ?>/assets/img/apple-touch-icon-57x57.png">
+  <link rel="apple-touch-icon" sizes="57x57" href="<?php echo get_stylesheet_directory_uri() ?>/assets/img/apple-touch-icon-72x72.png">
+  <link rel="apple-touch-icon" sizes="76x76" href="<?php echo get_stylesheet_directory_uri() ?>/assets/img/apple-touch-icon-76x76.png">
+  <link rel="apple-touch-icon" sizes="114x114" href="<?php echo get_stylesheet_directory_uri() ?>/assets/img/apple-touch-icon-114x114.png">
+  <link rel="apple-touch-icon" sizes="120x120" href="<?php echo get_stylesheet_directory_uri() ?>/assets/img/apple-touch-icon-120x120.png">
+  <link rel="apple-touch-icon" sizes="144x144" href="<?php echo get_stylesheet_directory_uri() ?>/assets/img/apple-touch-icon-144x144.png">
+  <link rel="apple-touch-icon" sizes="152x152" href="<?php echo get_stylesheet_directory_uri() ?>/assets/img/apple-touch-icon-152x152.png">
   <?php
 }
 
