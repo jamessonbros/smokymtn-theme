73acb55ac7f41ad52a11602a100dde1c006e9dea
Steve Jamesson <steve@groovywebdesign.com>
2014-02-17 12:04:35 -0500

    update footer

--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '314dbca4298b343445cca6f3e778f009');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'a3f63bc366917a885a44ae0f7581fc9f');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
--- templates/footer.php
+++ templates/footer.php
@@ -1,8 +1,17 @@
 <footer class="content-info container" role="contentinfo">
   <div class="row">
-    <div class="col-lg-12">
-      <?php dynamic_sidebar('sidebar-footer'); ?>
-      <p>&copy; <?php echo date('Y'); ?> <?php bloginfo('name'); ?></p>
+    <div class="col-xs-12 col-sm-4 col-md-4 col-lg-4">
+      <p class="copyright">&copy; <?php echo date('Y'); ?> <a href="<?php echo home_url() ?>"><?php bloginfo('name'); ?></a></p>
+    </div>
+    <div class="col-xs-12 col-sm-4 col-md-4 col-lg-4">
+      <div class="footer-mid">
+        #affiliated_logos
+        <br>
+        #socials
+      </div>
+    </div>
+    <div class="col-xs-12 col-sm-4 col-md-4 col-lg-4">
+      <p class="credit">Site by <a href="http://www.groovywebdesign.com/" title="Make your website great!" target="_blank"><img src="http://wpassets.groovywebdesign.com/gwd-foot-logo-white.png" alt="Groovy Web Design" width="30"></a></p>
     </div>
   </div>
 </footer>
