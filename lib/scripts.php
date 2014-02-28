55d7e2ce1a6109b7debec349bd11587750f7c9f8
Steve Jamesson <steve@groovywebdesign.com>
2014-02-28 14:51:24 -0500

    progress w/ new direction

--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '686e21158f9f6f84d8e95ba3840602dd');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '4f2fd43f721c6dba286425127d29908f');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
--- templates/header-top-navbar.php
+++ templates/header-top-navbar.php
@@ -1,21 +1,58 @@
-<header class="banner navbar navbar-inverse navbar-static-top" role="banner">
-  <div class="container">
-    <div class="navbar-header">
-      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-        <span class="sr-only">Toggle navigation</span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-      </button>
-      <a class="navbar-brand" href="<?php echo home_url(); ?>/"><?php bloginfo('name'); ?></a>
+<header class="site-header">
+  <div class="stripe">
+    <div class="container">
+      <div class="row">
+        <div class="col-xs-12 col-sm-9 col-md-9 col-lg-9">
+          <div class="nav-container">
+            <?php if (has_nav_menu('top_navigation')) {
+              wp_nav_menu(array(
+                'theme_location' => 'top_navigation',
+                'container' => false,
+                'menu_class' => 'list-inline',
+                'menu_id' => 'top-nav',
+              ));
+            } ?>
+          </div>
+        </div>
+        <div class="col-xs-12 col-sm-3 col-md-3 col-lg-3">
+          <div class="nav-container">
+            <?php if (has_nav_menu('top_right_navigation')) {
+              wp_nav_menu(array(
+                'theme_location' => 'top_right_navigation',
+                'container' => false,
+                'menu_class' => 'list-inline',
+                'menu_id' => 'top-right-nav',
+              ));
+            } ?>
+          </div>
+        </div>
+      </div>
     </div>
-
-    <nav class="collapse navbar-collapse" role="navigation">
-      <?php
-        if (has_nav_menu('primary_navigation')) :
-          wp_nav_menu(array('theme_location' => 'primary_navigation', 'menu_class' => 'nav navbar-nav'));
-        endif;
-      ?>
-    </nav>
+  </div>
+  <!-- /stripe -->
+  <div class="banner">
+    <div class="container">
+      <div class="row">
+        <div class="col-xs-12 col-sm-4 col-md-4 col-lg-4">
+          <div class="logo-container">
+            <a href="<?php echo home_url() ?>" title="<?php bloginfo('name') ?>">
+              <img src="<?php echo get_stylesheet_directory_uri() ?>/assets/img/logo.png" alt="<?php bloginfo('name') ?>" class="img-responsive">
+            </a>
+          </div>
+        </div>
+        <div class="col-xs-12 col-sm-8 col-md-8 col-lg-8">
+          <div class="nav-container">
+            <?php if (has_nav_menu('primary_navigation')) {
+              wp_nav_menu(array(
+                'theme_location' => 'primary_navigation',
+                'container' => false,
+                'menu_class' => 'list-inline',
+                'menu_id' => 'primary-nav',
+              ));
+            } ?>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </header>
