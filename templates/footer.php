e17777ddc266ce5ce8ead258d074de36015e2692
Steve Jamesson <steve@groovywebdesign.com>
2014-03-01 10:28:47 -0500

    tweak footer container

--- templates/footer.php
+++ templates/footer.php
@@ -1,19 +1,23 @@
-<footer class="content-info container" role="contentinfo">
-  <div class="row">
-    <div class="col-xs-12 col-sm-4 col-md-4 col-lg-4">
-      <p class="copyright">&copy; <?php echo date('Y'); ?> <a href="<?php echo home_url() ?>"><?php bloginfo('name'); ?></a></p>
-    </div>
-    <div class="col-xs-12 col-sm-4 col-md-4 col-lg-4">
-      <div class="footer-mid">
-        #affiliated_logos
-        <br>
-        #socials
+<footer class="content-info" role="contentinfo">
+  <div class="container">
+    <div class="row">
+      <div class="col-xs-12 col-sm-4 col-md-4 col-lg-4">
+        <p class="copyright">&copy; <?php echo date('Y'); ?> <a href="<?php echo home_url() ?>"><?php bloginfo('name'); ?></a></p>
+      </div>
+      <div class="col-xs-12 col-sm-4 col-md-4 col-lg-4">
+        <div class="footer-mid">
+          #affiliated_logos
+          <br>
+          #socials
+        </div>
+      </div>
+      <div class="col-xs-12 col-sm-4 col-md-4 col-lg-4">
+        <p class="credit">Site by <a href="http://www.groovywebdesign.com/" title="Make your website great!" target="_blank"><img src="http://wpassets.groovywebdesign.com/gwd-foot-logo-white.png" alt="Groovy Web Design" width="30"></a></p>
       </div>
     </div>
-    <div class="col-xs-12 col-sm-4 col-md-4 col-lg-4">
-      <p class="credit">Site by <a href="http://www.groovywebdesign.com/" title="Make your website great!" target="_blank"><img src="http://wpassets.groovywebdesign.com/gwd-foot-logo-white.png" alt="Groovy Web Design" width="30"></a></p>
-    </div>
+    <!-- /row -->
   </div>
+  <!-- /container -->
 </footer>
 
 <?php wp_footer(); ?>
