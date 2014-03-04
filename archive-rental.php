53bba96d59e964c8058647b4aae41dbeea2fed88
Steve Jamesson <steve@groovywebdesign.com>
2014-03-04 14:44:33 -0500

    fix rental_type term, was only showing description across all bedroom queries

--- archive-rental.php
+++ archive-rental.php
@@ -3,3 +3,5 @@
 <?php while (have_posts()) : the_post(); ?>
   <?php get_template_part('templates/content', 'rental'); ?>
 <?php endwhile; ?>
+
+<?php get_template_part('templates/pagination') ?>
--- taxonomy-rental_types.php
+++ taxonomy-rental_types.php
@@ -1,13 +1,20 @@
-<?php get_template_part('templates/page', 'header') ?>
-
 <?php 
+$bedrooms = get_query_var('bedrooms');
 $term_obj = get_term_by('slug', $term, 'rental_types');
 $descr = term_description($term_obj->term_id, 'rental_types');
 ?>
 
-<?php if ($descr): ?>
-<div class="term-description">
-  <?php echo term_description($term_obj->term_id, 'rental_types') ?>
-</div>
-<!-- /term-description -->
+<?php if (!$bedrooms && $descr): ?>
+
+  <?php get_template_part('templates/page', 'header') ?>
+
+  <div class="term-description">
+    <?php echo term_description($term_obj->term_id, 'rental_types') ?>
+  </div>
+  <!-- /term-description -->
+
+<?php else: ?>
+
+  <?php get_template_part('archive-rental') ?>
+
 <?php endif ?>
