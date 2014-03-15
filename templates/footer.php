af73701329a600a6ddc5e96d0c40e1d66c7e32ed
Steve Jamesson <steve@groovywebdesign.com>
2014-03-15 11:08:48 -0400

    affil logos, member commerce

--- templates/footer.php
+++ templates/footer.php
@@ -1,13 +1,21 @@
 <footer class="content-info" role="contentinfo">
   <div class="container">
     <div class="row">
+      <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 text-center">
+        <p>Member of Jackson, Macon, Haywood, and Swain County Chambers of Commerce</p>
+      </div>
+      <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 text-center">
+        <img src="<?php echo get_stylesheet_directory_uri() ?>/assets/img/equal-housing.jpg" alt="Equal Housing Opportunity">
+        <img src="<?php echo get_stylesheet_directory_uri() ?>/assets/img/mls.jpg" alt="MLS">
+        <img src="<?php echo get_stylesheet_directory_uri() ?>/assets/img/ncvrma.jpg" alt="NCVRMA">
+      </div>
+    </div>
+    <div class="row">
       <div class="col-xs-12 col-sm-4 col-md-4 col-lg-4">
         <p class="copyright">&copy; <?php echo date('Y'); ?> <a href="<?php echo home_url() ?>"><?php bloginfo('name'); ?></a></p>
       </div>
       <div class="col-xs-12 col-sm-4 col-md-4 col-lg-4">
         <div class="footer-mid">
-          #affiliated_logos
-          <br>
           #socials
         </div>
       </div>
